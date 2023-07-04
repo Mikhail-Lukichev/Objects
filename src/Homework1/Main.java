907d75065cb5381485c849f850fc4ffc0bb8971d
Mikhail Lukichev <dextrumm@gmail.com>
2023-07-03 21:59:06 -0600

    Homework1

--- src/Homework1/Main.java
+++ src/Homework1/Main.java
@@ -2,6 +2,13 @@ package Homework1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1");
+        Author leoTolstoy = new Author("Leo","Tolstoy");
+        Author fyodorDostoevsky = new Author("Fyodor","Dostoevsky");
+        Book warAndPeace = new Book("War And Peace",leoTolstoy,1869);
+        Book crimeAndPunishment = new Book("Crime And Punishment",fyodorDostoevsky,1866);
+        System.out.println("warAndPeace.getPublishingYear() = " + warAndPeace.getPublishingYear());
+        warAndPeace.setPublishingYear(1870);
+        System.out.println("warAndPeace.getPublishingYear() = " + warAndPeace.getPublishingYear());
     }
 }
